c9105c709934a4a38520dfd9bcf47dfd460d7ad1
Kyd <kyd.kitchaiya@medieinstitutet.se>
2022-01-23 22:33:19 +0100

    feat: install cypress, test login logout

---
+++ cypress/integration/loginLogout.js
@@ -0,0 +1,16 @@
+describe("Test sign in", function () {
+  it("Signs in", function () {
+    cy.visit("http://localhost:3000");
+
+    cy.get("#email").type("k@gmail.com");
+    cy.get("#password").type("test1234{enter}");
+  });
+
+  it("greets user", function () {
+    cy.get(".greet").contains("Hi, you are logged in as k@gmail.com");
+  });
+
+  it("logs out", function () {
+    cy.get(".nav-logout").click();
+  });
+});
