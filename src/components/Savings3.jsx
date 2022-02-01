cf019deaf58817237e104aa198b0d3aa5937b83f
Kyd <kyd.kitchaiya@medieinstitutet.se>
2022-02-01 23:56:45 +0100

    fix: disable confetti

--- src/components/Savings3.jsx
+++ src/components/Savings3.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState } from "react";
 //fire
 import { doc, updateDoc } from "firebase/firestore";
 import { db } from "../firebase";
